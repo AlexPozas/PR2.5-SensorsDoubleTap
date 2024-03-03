d5bf4c562506a92389e49fcef2bf80a830d3bf21
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2024-03-03 11:00:21 +0100

    FET

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -1,52 +1,56 @@
 package com.example.sensors;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
+import android.view.GestureDetector;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView ;
-    TextView textView1 ;
-    TextView textView2 ;
+
     private SensorManager sensorManager;
     private Sensor sensor;
     SensorEventListener sensorListener;
+    TextView x, y, z;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-        textView1 = findViewById(R.id.textView2);
-        textView2 = findViewById(R.id.textView3);
+
+        final Context context = this;
+
+        x = findViewById(R.id.x);
+        y = findViewById(R.id.y);
+        z = findViewById(R.id.z);
+
         sensorListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                // Valors de l'acceleròmetre en m/s^2
                 float xAcc = sensorEvent.values[0];
                 float yAcc = sensorEvent.values[1];
                 float zAcc = sensorEvent.values[2];
-                textView.setText("" + xAcc);
-                textView1.setText("" + yAcc);
-                textView2.setText("" + zAcc);
 
-                // Processament o visualització de dades...
-
+                x.setText(Float.toString(xAcc));
+                y.setText(Float.toString(yAcc));
+                z.setText(Float.toString(zAcc));
             }
 
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
-
-
+                // Es pot ignorar aquesta CB de moment
             }
         };
 
-
-
+        // Seleccionem el tipus de sensor (veure doc oficial)
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
@@ -55,5 +59,28 @@ public class MainActivity extends AppCompatActivity {
             sensorManager.registerListener(sensorListener,sensor,
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
+
+        final GestureDetector.SimpleOnGestureListener listener = new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                Toast.makeText(context, " DoubleTap!", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+        };
+
+        final GestureDetector detector = new GestureDetector(listener);
+
+        detector.setOnDoubleTapListener(listener);
+        detector.setIsLongpressEnabled(true);
+
+        getWindow().getDecorView().setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                return detector.onTouchEvent(event);
+            }
+        });
     }
+
+
 }
